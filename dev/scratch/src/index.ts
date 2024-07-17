658f0f3516159f2c4d2559ad7b41c663c6c47f07
dereckmezquita <dereck@mezquita.io>
2024-07-17 11:11:02 -0500

    STAT-4: Basic and working allows for query method to work.

---
+++ dev/scratch/src/index.ts
@@ -0,0 +1,24 @@
+import { DataTable } from './DataTable/DT_col';
+
+interface Person {
+    id: number;
+    name: string;
+    age: number;
+}
+
+const DT = new DataTable<Person>([
+    { id: 1, name: 'Alice', age: 30 },
+    { id: 2, name: 'Bob', age: 25 },
+    { id: 3, name: 'Charlie', age: 35 },
+    { id: 4, name: 'David', age: 20 },
+    { id: 5, name: 'Eve', age: 15 },
+    { id: 6, name: 'Frank', age: 40 }
+]);
+
+// R: DT[age >= 18, .(name, age)]
+// const adults: DataTable<Person>
+const adults = DT.query((row) => row.age >= 10 && row.age < 30, {
+    select: ['name', 'age']
+});
+
+console.log(adults);
