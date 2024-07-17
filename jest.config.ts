f51cf9573bbe5cfcfebd857d812337851044f91f
dereckmezquita <dereck@mezquita.io>
2024-07-17 10:01:13 -0500

    STAT-4: Only get tests from test folder.

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     // Specify test files pattern
-    testMatch: ['**/test/**/*.ts', '**/?(*.)+(spec|test).ts'],
+    testMatch: ['./test/**/*.ts', '**/?(*.)+(spec|test).ts'],
     // Module file extensions for importing
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
